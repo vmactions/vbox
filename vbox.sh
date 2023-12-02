abec88546eed83f30bb3f59f7919fa257e1b647b
neil <neilgit@neilpang.com>
2023-12-02 10:10:08 +0100

    add VM_OCR

--- vbox.sh
+++ vbox.sh
@@ -296,23 +296,39 @@ attachISO() {
 #img
 _ocr() {
   _ocr_img="$1"
-  tesseract -l eng $_ocr_img - 2>/dev/null
-#  pytesseract $_ocr_img
-#  python3 -c "
-#import cv2,pytesseract,numpy,sys;
-#img = cv2.imread(sys.argv[1]);
-#gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY);
-#gray, img_bin = cv2.threshold(gray,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU);
-#gray = cv2.bitwise_not(img_bin);
-#kernel = numpy.ones((2, 1), numpy.uint8);
-#img = cv2.erode(gray, kernel, iterations=1);
-#img = cv2.dilate(img, kernel, iterations=1);
-#out_below = pytesseract.image_to_string(img);
-#print(out_below);
-#
-#"  "$_ocr_img"
-
+  if [ "$VM_OCR" == "py" ]; then
+    _ocrpy "$_ocr_img"
+  else
+    _ocrt "$_ocr_img"
+  fi
 }
+
+
+#img
+_ocrt() {
+  _ocr_img="$1"
+  tesseract -l eng $_ocr_img - 2>/dev/null
+}
+
+#img
+_ocrpy() {
+  _ocr_img="$1"
+  #pytesseract $_ocr_img
+  python3 -c "
+import cv2,pytesseract,numpy,sys;
+img = cv2.imread(sys.argv[1]);
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY);
+gray, img_bin = cv2.threshold(gray,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU);
+gray = cv2.bitwise_not(img_bin);
+kernel = numpy.ones((2, 1), numpy.uint8);
+img = cv2.erode(gray, kernel, iterations=1);
+img = cv2.dilate(img, kernel, iterations=1);
+out_below = pytesseract.image_to_string(img);
+print(out_below);
+
+"  "$_ocr_img"
+}
+
 
 #osname [img]
 screenText() {
